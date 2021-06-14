83b2c94a29da28261d9e1137319b30b741d81f18
SpaceCowboyS01 <77464949+SpaceCowboyS01@users.noreply.github.com>
2021-06-14 18:28:57 +0200

    Generatore di file.txt
    Per rendere la raccolta dati prolifica, abbiamo scritto questo breve codice python per generare dei file.txt con un incremento di 10 caratteri per volta, ottenendo così 10 fai con dimensione (in B):
    1, 11, 21, 31, 41, 51, 61, 71, 81, 91, 101.

---
+++ Generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from string import printable
+from random import choice
+
+alf = printable[:-5]
+
+
+def main():
+    for i in range(1, 110, 10):
+        with open('input_'+str(i)+'.txt', 'w') as f:
+            f.write(''.join([choice(alf) for _ in range(i)]))
+
+
+if __name__ == '__main__':
+    main()
