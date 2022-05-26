449001e01c44820824732f8bf0c6f87648182c5b
Simone Cisca <77464949+SpaceCowboyS01@users.noreply.github.com>
2022-05-26 12:02:19 +0200

    Code readability improved

--- Generator.py
+++ Generator.py
@@ -4,12 +4,10 @@ from random import choice
 
 alf = printable[:-5]
 
-
 def main():
     for i in range(1, 110, 10):
         with open('input_'+str(i)+'.txt', 'w') as f:
             f.write(''.join([choice(alf) for _ in range(i)]))
-
 
 if __name__ == '__main__':
     main()
